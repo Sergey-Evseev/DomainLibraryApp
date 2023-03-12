433ce2787b77beb54be06401a1c7ef8fcc0f825e
Sergey Evseev <sevseev@gmail.com>
2023-03-12 22:57:00 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLibraryApp
+{
+    public class SampleClass
+    {
+        public static void DoSome()
+        {
+            Console.WriteLine("Этот статический метод выполняемтся из DomainLibraryApp");
+        }
+    }
+}
