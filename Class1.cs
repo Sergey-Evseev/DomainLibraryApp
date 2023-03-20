9269003a09783651468509e80dc73a550165a26e
Sergey Evseev <sevseev@gmail.com>
2023-03-21 00:35:00 +0300

    One line method

--- Class1.cs
+++ Class1.cs
@@ -8,9 +8,11 @@ namespace DomainLibraryApp
 {
     public class SampleClass
     {
+        public static int a = 5;
         public static void DoSome()
         {
-            Console.WriteLine("Этот статический метод выполняется из DomainLibraryApp");
+            Console.WriteLine("Этот статический **** метод выполняется из DomainLibraryApp");
+            a++;
         }
     }
 }
